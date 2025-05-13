7d6d2bb1f2e4439e273798d595a9f39d58274fa4
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2025-05-12 22:32:16 -0300

    Muda chaves primárias de usuário e loja

--- banco/setup_db.sql
+++ banco/setup_db.sql
@@ -1,7 +1,5 @@
 -- Tabela de lojas: nome, data de criação, cidade, UF, descrição
 CREATE TABLE IF NOT EXISTS Loja (
-  id INTEGER AUTO_INCREMENT,
-
   nome TEXT NOT NULL,
   dia_criacao INTEGER NOT NULL,
   mes_criacao INTEGER NOT NULL,
@@ -9,18 +7,16 @@ CREATE TABLE IF NOT EXISTS Loja (
   cidade TEXT NOT NULL,
   estado TEXT NOT NULL,
   descricao TEXT NOT NULL,
-
-  PRIMARY KEY (id)
+  PRIMARY KEY (nome)
 );
 
 -- Tabela de usuários: nome, senha, loja
 CREATE TABLE IF NOT EXISTS Usuario (
-  id INTEGER AUTO_INCREMENT,
   nome TEXT NOT NULL,
   senha TEXT NOT NULL,
-  loja INTEGER, -- pode ser nulo; usuário pode não ter uma loja ainda
-  PRIMARY KEY (id),
-  FOREIGN KEY (loja) REFERENCES Loja (id)
+  loja TEXT, -- opcional
+  PRIMARY KEY (nome),
+  FOREIGN KEY (loja) REFERENCES Loja (nome)
 );
 
 --------------------------------------------------------------------------------
@@ -61,20 +57,20 @@ CREATE TABLE IF NOT EXISTS Maquina (
   FOREIGN KEY (modelo) REFERENCES Modelo (id)
 );
 
-INSERT INTO `Loja` (`id`, `nome`, `dia_criacao`, `mes_criacao`, `ano_criacao`, `cidade`, `estado`, `descricao`) VALUES
-  (1, 'adminStore', 12, 2, 2004, 'Fernandópolis', 'SP', 'Loja de colheitadeiras de café');
+INSERT INTO Loja (nome, dia_criacao, mes_criacao, ano_criacao,
+  cidade, estado, descricao) VALUES
+  ('adminStore', 12, 2, 2004,
+    'Fernandópolis', 'SP', 'Loja de colheitadeiras de café');
 
-INSERT INTO `Usuario` (`nome`, `senha`, `loja` ) VALUES
-  ('admin', 'admin', 1 );
+INSERT INTO Usuario (nome, senha, loja ) VALUES
+  ('admin', 'admin', 'adminStore' );
 
-INSERT INTO `Empresa` (`nome`) VALUES
+INSERT INTO Empresa (nome) VALUES
   ('CAT');
 
-INSERT INTO `Modelo` (`fabricante`, `nome`, `descricao`) VALUES
-  ('CAT', 'Challenger MT525D 4WD', 'Linha de produto:	tratores agrícolas, Número de série	D049045, Número de catálogo	473575');
+INSERT INTO Modelo (fabricante, nome, descricao) VALUES
+  ('CAT', 'Challenger MT525D 4WD',
+    'Linha de produto:	tratores agrícolas, Número de série	D049045, Número de catálogo	473575');
 
-INSERT INTO `Maquina` (`loja`, `modelo`, `imagem`, `preco`, `mes_fabricacao`, `ano_fabricacao`) VALUES
+INSERT INTO Maquina (loja, modelo, imagem, preco, mes_fabricacao, ano_fabricacao) VALUES
   (1, 1, 'static/CAT1.png', 394.23, 2, 2013);
-
-  -- Horas de utilização	5.200 h 
-  -- Localização da máquina	Mexicali, Mexico
